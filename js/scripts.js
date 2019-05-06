fe897fe63e36c0f1a0703579116a7e41f6074300
Beryl Onyancha <berylonyancha@gmail>
2019-05-06 09:48:20 +0300

    Add alert after message has beem submitted

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
     $(this).children(".text-showing").toggle();
     $(this).children(".text-hidden").toggle();
   });
+  $("#submit").click(function(event){
+    var dataName =$("#name1").val();
+    alert("Thank you" + dataName + "for reaching out to us");
+    event.preventDefault();
+  });
+
 });
+
 
 
 
